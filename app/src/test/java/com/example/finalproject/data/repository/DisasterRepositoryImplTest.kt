c33186a7b31c994942c87fb84020b71da88dbd22
yan <darmansyahyan21@gmail.com>
2023-08-11 11:18:47 +0800

    test repository get disaster

--- app/src/test/java/com/example/finalproject/data/repository/DisasterRepositoryImplTest.kt
+++ app/src/test/java/com/example/finalproject/data/repository/DisasterRepositoryImplTest.kt
@@ -1,18 +1,52 @@
 package com.example.finalproject.data.repository
 
+import com.example.finalproject.data.ApiService
+import com.example.finalproject.data.fakeRepository.FakeData
+import com.example.finalproject.data.model.ResponseData
+import com.example.finalproject.data.model.Result
 import com.example.finalproject.domain.repository.DisasterRepository
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import retrofit2.Response
 
-class DisasterRepositoryImplTest{
+class DisasterRepositoryImplTest {
+
+
+    @Mock
+    private lateinit var mockApiService: ApiService
 
     private lateinit var disasterRepository: DisasterRepository
 
 
-
-
     @Before
-    fun setUp(){
-
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        disasterRepository = DisasterRepositoryImpl(mockApiService)
     }
 
+    @Test
+    fun `test repository getDisaster success`() {
+        runBlocking {
+            // Mocking the ApiService response
+            val mockResponse: Response<ResponseData> = mockResponseWithData()
+            `when`(mockApiService.getReport("26135")).thenReturn(mockResponse)
+
+            // Call the repository method
+            val result = disasterRepository.getDisaster("26133")
+
+            // Verify the result
+            assert(result.isSuccessful)
+            assert(result.body() != null)
+            assert(result.body()?.result != null)
+        }
+    }
+
+    private fun mockResponseWithData(): Response<ResponseData> {
+        val responseData = FakeData.fakeResponseData
+        return Response.success(responseData)
+    }
 }
